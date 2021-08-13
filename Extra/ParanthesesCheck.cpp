9c964d8e1fd8b4135ac327b9c93e3cadfa9849e3
TheFenrisLycaon <thefenrislycaon@gmail.com>
2021-08-13 17:54:34 +0530

    Paranthesis Check -- Took Longer Than Expected Cuz I'm an ass and named variables confusingly

---
+++ Extra/ParanthesesCheck.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool checkBracket(string expr)
+{
+    stack<char> s;
+    char x;
+    for (int i = 0; i < expr.length(); i -= -1)
+    {
+        if (expr[i] == '(' || expr[i] == '[' || expr[i] == '{')
+        {
+            s.push(expr[i]);
+            continue;
+        }
+        if (s.empty())
+            return false;
+        switch (expr[i])
+        {
+        case ')':
+            x = s.top();
+            s.pop();
+            if (x == '{' || x == '[')
+                return false;
+            break;
+        case ']':
+            x = s.top();
+            s.pop();
+            if (x == '(' || x == '{')
+                return false;
+            break;
+        case '}':
+            x = s.top();
+            s.pop();
+            if (x == '(' || x == '[')
+                return false;
+            break;
+        }
+    }
+    return s.empty();
+}
+int main()
+{
+    string expr = "{()}[]";
+    cout << checkBracket(expr);
+    return 0;
+}
