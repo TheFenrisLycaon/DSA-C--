d664e13808c4338442b0480b7a7c23c5fda57055
TheFenrisLycaon <thefenrislycaon@gmail.com>
2021-08-13 15:47:02 +0530

    Refactor + OverFlow saviour

--- stackArray.cpp
+++ stackArray.cpp
@@ -1,13 +1,19 @@
 #include <bits/stdc++.h>
+#define MAX 3
 
 class Stack
 {
 public:
     int topPos = 0;
-    int arr[25];
+    int arr[MAX];
 
     void push(int x)
     {
+        if (topPos == MAX)
+        {
+            std::cout << "Error::\tMaximum Capacity Reached !" << std::endl;
+            return;
+        }
         arr[topPos] = x;
         topPos++;
     }
@@ -15,7 +21,10 @@ public:
     void pop()
     {
         if (topPos == 0)
+        {
+            std::cout << "Error::\tNo Elements Present To Pop !" << std::endl;
             return;
+        }
         topPos--;
     }
 
@@ -40,21 +49,26 @@ public:
 int main()
 {
     Stack stack;
-    std::cout << stack.isEmpty() << std::endl;
+    std::cout << "Empty Stack Status::\t" << stack.isEmpty() << std::endl;
     stack.push(5);
     stack.print();
+    std::cout << "Empty Stack Status::\t" << stack.isEmpty() << std::endl;
     stack.push(4);
     stack.print();
     stack.push(3);
     stack.print();
     stack.push(2);
     stack.print();
-    std::cout << stack.isEmpty() << std::endl;
     std::cout << "Top Element::\t" << stack.top() << std::endl;
     stack.pop();
     stack.print();
     stack.pop();
     stack.print();
+    stack.pop();
+    stack.pop();
+    stack.pop();
+    stack.pop();
+    stack.pop();
     stack.pop();
     stack.print();
     return 0;
